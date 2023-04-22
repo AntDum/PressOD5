147b4eadaa9222b74a06b5c6b13ed1dbfcf5d763
AntDum <antoine.dumoulin@student.uclouvain.be>
2023-04-22 21:11:02 +0200

    pushable

--- script/enemy/pushable.gd
+++ script/enemy/pushable.gd
@@ -1,16 +1,26 @@
 extends KinematicBody2D
 
+var direction
+var start_pos
+export var speed = 150
+export var limit = 500
 
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
-
-
-# Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	set_physics_process(false)
 
+func _on_HurtBox_area_entered(area):
+	var player = area.get_parent()
+	if player.has_method("get_direction"):
+		direction = player.get_direction()
+		set_physics_process(true)
+		start_pos = position
+		
+		
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
+func _physics_process(delta):
+	if (position.distance_to(start_pos) > limit):
+		set_physics_process(false)
+		return
+	var velocity = move_and_slide(direction * speed)
+	if (velocity.length() < 5):
+		set_physics_process(false)
