b6e1333ff27075975739f4b0eff1d4ca077bc839
tschoenauen <thibaud.schoenauen@student.uclouvain.be>
2023-04-22 16:19:38 +0200

    merging commit

--- script/levels/1_Woodstock.gd
+++ script/levels/1_Woodstock.gd
@@ -4,9 +4,6 @@ extends Node
 signal change_scene(to)
 
 func _ready():
-	var aggressivity = PlayerInfo.aggressivity
-	spawn_enemies(aggressivity)
+	pass
 
 
-func spawn_enemies(aggressivity):
-	return null
