b4b86fb87d35e9a36e0fc3e990ec98d7903b1f05
AntDum <antoine.dumoulin@student.uclouvain.be>
2023-04-22 22:38:12 +0200

    dialog_trigger

--- script/prefabs/dialog_trigger.gd
+++ script/prefabs/dialog_trigger.gd
@@ -2,12 +2,22 @@ extends Area2D
 
 signal dialogic_signal(message)
 export var timeline := ""
+onready var player = $"%Player"
 
-func _on_Area2D_area_entered(area):
-	$"%Player".state = $"%Player".STATE_NULL
+func _on_Area2D_area_entered(_area):
+	trigger()
+
+func _on_dialogue_trigger_body_entered(_body):
+	trigger()
+
+func trigger():
+	player.state = player.STATE_NULL
 	var dialog = Dialogic.start(timeline)
 	add_child(dialog)
-	connect("dialogic_signal", dialog, "transfere_signal")
+	dialog.connect("dialogic_signal", self, "transfere_signal")
 
 func transfere_signal(message):
+	print("HELLO")
+	player.idle()
 	emit_signal("dialogic_signal", message)
+	call_deferred("queue_free")
