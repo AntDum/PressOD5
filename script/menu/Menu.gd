e5f5fd536c8dfbedb06681d7b87ea7f02c6edb75
AntDum <antoine.dumoulin@student.uclouvain.be>
2023-04-22 18:12:51 +0200

    new scene manager

--- script/menu/Menu.gd
+++ script/menu/Menu.gd
@@ -4,6 +4,9 @@ signal change_scene(to)
 
 
 func _on_Play_pressed():
+	#var dialog = Dialogic.start("metal_interaction_1")
+	#var dialog = Dialogic.start("introduction")
+	#add_child(dialog)
 	emit_signal("change_scene", "res://scene/levels/1_Woodstock.tscn")
 
 
