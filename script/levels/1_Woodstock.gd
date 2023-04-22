66e8ed76a0531e8eff3440a00747fdd3bc46690e
tschoenauen <thibaud.schoenauen@student.uclouvain.be>
2023-04-22 22:02:34 +0200

    Added fences on WoodstockLVL

--- script/levels/1_Woodstock.gd
+++ script/levels/1_Woodstock.gd
@@ -4,10 +4,9 @@ extends Node
 signal change_scene(to)
 
 func _ready():
-	var dialog = Dialogic.start("introduction")
-	add_child(dialog)
-	$Player.state = $Player.STATE_NULL
-	$Arenas/Node2D.player = $Player
 	
-	dialog.connect("dialogic_signal", $Player, "idle")
+	
+	
+	$Arenas/Node2D.player = $Player
+
 
