753cdcef8e1eebec26b002c3c01fedf03bc6ee71
AntDum <antoine.dumoulin@student.uclouvain.be>
2023-04-22 14:23:51 +0200

    autoload

---
+++ script/autoload/SceneManager.gd
@@ -0,0 +1,16 @@
+extends Node
+
+var current_scene : Node
+
+func change_scene(to: String) -> void:
+	if current_scene:
+		current_scene.free()
+	current_scene = load(to).instance()
+	add_child(current_scene)
+	move_child(current_scene, 0)
+	if current_scene.has_signal("change_scene"):
+		current_scene.connect("change_scene", self, "change_scene", [], CONNECT_DEFERRED)
+
+
+func _ready() -> void:
+	call_deferred("change_scene", "res://scene/menu/Menu.tscn") 
