8d9dff2f6560a45b362cafae7f4f2d917aede504
AntDum <antoine.dumoulin@student.uclouvain.be>
2023-04-22 18:30:05 +0200

    changing police

--- script/menu/Menu.gd
+++ script/menu/Menu.gd
@@ -5,9 +5,9 @@ signal change_scene(to)
 
 func _on_Play_pressed():
 	#var dialog = Dialogic.start("metal_interaction_1")
-	#var dialog = Dialogic.start("introduction")
-	#add_child(dialog)
-	emit_signal("change_scene", "res://scene/levels/1_Woodstock.tscn")
+	var dialog = Dialogic.start("introduction")
+	add_child(dialog)
+	#emit_signal("change_scene", "res://scene/levels/1_Woodstock.tscn")
 
 
 func _on_Quit_pressed():
