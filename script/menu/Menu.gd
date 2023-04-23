e0088d75320cce0e476279b55f4a00df47275f76
AntDum <antoine.dumoulin@student.uclouvain.be>
2023-04-23 11:24:19 +0200

    k

--- script/menu/Menu.gd
+++ script/menu/Menu.gd
@@ -2,18 +2,18 @@ extends Control
 
 signal change_scene(to)
 
-func _ready():
-	$anims.play("start")
-
-func _on_Play_pressed():
-	#var dialog = Dialogic.start("metal_interaction_1")
-	#var dialog = Dialogic.start("introduction")
-	#add_child(dialog)
-	$anims.play("launch")
-	
-
-func _on_Quit_pressed():
-	get_tree().quit()
-
-func _play():
-	emit_signal("change_scene", "res://scene/levels/1_Woodstock.tscn")
+#func _ready():
+#	$anims.play("start")
+#
+#func _on_Play_pressed():
+#	#var dialog = Dialogic.start("metal_interaction_1")
+#	#var dialog = Dialogic.start("introduction")
+#	#add_child(dialog)
+#	$anims.play("launch")
+#
+#
+#func _on_Quit_pressed():
+#	get_tree().quit()
+#
+#func _play():
+#	emit_signal("change_scene", "res://scene/levels/1_Woodstock.tscn")
