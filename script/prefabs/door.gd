1c3f192a8e27aa3a1d0504cd1efd7ba07b18c081
tschoenauen <thibaud.schoenauen@student.uclouvain.be>
2023-04-23 12:43:20 +0200

    Liverpool

--- script/prefabs/door.gd
+++ script/prefabs/door.gd
@@ -6,9 +6,32 @@ func flip():
 	enable = not enable
 	$body/shape.set_deferred("disabled", not enable)
 
+func open():
+	enable = false
+	$body/shape.set_deferred("disabled", true)
+	$Sprite.set_deferred("visible",false)
+	
+	
+func close():
+	enable = true
+	$body/shape.set_deferred("disabled", false)
+	$Sprite.set_deferred("visible",true)
+
 func _on_lever_activated():
 	flip()
 
 
 func _on_Arena_arenaCleared():
 	flip()
+	
+func _on_goodLever_activated():
+	print("Openning door")
+	open()
+	
+func _on_badLever_activated():
+	print("Closing door")
+	close()
+
+
+func _on_wrongLever_activated():
+	pass # Replace with function body.
